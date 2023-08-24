fbb1f751209249a642e12fbf4c8b1eb50d076e76
vietpham2k3 <vietvuavanvat13579@gmail.com>
2023-08-24 22:46:04 +0700

    up

--- src/main/java/com/example/demo/controller/TestCon.java
+++ src/main/java/com/example/demo/controller/TestCon.java
@@ -8,6 +8,16 @@ public class TestCon {
 
     @GetMapping("/index")
     public String index(){
+        return "/pages/tables";
+    }
+
+    @GetMapping("/index1")
+    public String index1(){
         return "/pages/billing";
+    }
+
+    @GetMapping("/index2")
+    public String index2(){
+        return "/pages/hoa_don";
     }
 }
