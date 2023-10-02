764444e39ee14c9a9d64a018240f20772e61a079
dunngadz123 <143099440+dunngadz123@users.noreply.github.com>
2023-10-03 01:08:23 +0700

    fix

--- Front-end/src/views/sanpham/SanPham.js
+++ Front-end/src/views/sanpham/SanPham.js
@@ -1,13 +1,11 @@
 import Content from 'ui-component/sanpham/Content';
 import Footer from 'ui-component/trangchu/Footer';
 import Header from 'ui-component/trangchu/Header';
-import HeaderBottom from 'ui-component/trangchu/HeaderBottom';
 
 function SanPham() {
   return (
     <div>
       <Header />
-      <HeaderBottom />
       <Content />
       <Footer />
     </div>
