7ab47d75bb703e696b6a672fd94668a59982f04d
DESKTOP-RC17L00\Admin <phuttph23790@fpt.edu.vn>
2023-10-03 00:57:52 +0700

    sanpham/web

--- Front-end/src/App1.js
+++ Front-end/src/App1.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router';
 import TrangChu from 'views/home/TrangChu';
+import SanPham from 'views/sanpham/SanPham';
 
 const App1 = () => {
   return (
     <Routes>
       <Route path="/trang-chu" element={<TrangChu />}></Route>
+      <Route path="/san-pham/web" element={<SanPham />}></Route>
     </Routes>
+    
   );
 };
 
