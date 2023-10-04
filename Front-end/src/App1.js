d33b4b83ca985e59d52e2295f236a10d1a909763
namntph23252 <namntph23252@fpt.edu.vn>
2023-10-05 00:29:48 +0700

    3

--- Front-end/src/App1.js
+++ Front-end/src/App1.js
@@ -7,11 +7,10 @@ const App1 = () => {
   return (
     <Routes>
       <Route path="/trang-chu" element={<TrangChu />}></Route>
-<<<<<<< HEAD
+
       <Route path="/cart" element={<Cart />}></Route>
-=======
+
       <Route path="/san-pham/web" element={<SanPham />}></Route>
->>>>>>> 3ee8b915a9bebf1006f1c3055beb2bed8026789b
     </Routes>
   );
 };
