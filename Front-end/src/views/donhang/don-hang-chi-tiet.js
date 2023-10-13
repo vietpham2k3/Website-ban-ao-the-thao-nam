431ced1197879d183fc8c3ee4eb326d9e3268c1a
vietpham2k3 <vietvuavanvat13579@gmail.com>
2023-10-13 23:00:52 +0700

    up

--- Front-end/src/views/donhang/don-hang-chi-tiet.js
+++ Front-end/src/views/donhang/don-hang-chi-tiet.js
@@ -1781,8 +1781,9 @@ function DonHangCT() {
                                               onChange={() => handleDetail(d.id)}
                                             />
                                             <label className="form-check-label custom-label" htmlFor={d.id}>
-                                              <div style={{ backgroundColor: d.mauSac.ten, width: 50, borderRadius: '10px' }}>&nbsp;</div>
-                                              &nbsp;- {d.kichCo.ten}
+                                            <div style={{ backgroundColor: d.mauSac.ten, width: 50, borderRadius: '10px' }}>&nbsp;</div>
+                                              &nbsp;- {d.kichCo.ten} - {d.chatLieu.ten} - {d.loaiSanPham.ten} - {d.coAo.ten} -{' '}
+                                              {d.nhaSanXuat.ten}
                                             </label>
                                           </div>
                                         ))}
